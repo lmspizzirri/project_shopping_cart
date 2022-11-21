bb25b2a5e9ffebe4485df301bb29578063e9d2a5
lmspizzirri <lm.spizzirri@gmail.com>
2022-11-21 06:47:15 -0500

    Req 3

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 import { searchCep } from './helpers/cepFunctions';
 import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
 import './style.css';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
-fetchProductsList('computador');
+const criacaoProdutos = async () => {
+    //Recebendo as info dos computadores
+  const info = await fetchProductsList('computador');
+    //Selecionado a section dos produtos
+  const products = document.querySelector('.products');
+    //Adicionando dentro da section, os elementos com as info dos produtos
+  info.forEach((element) => products.appendChild(createProductElement(element)));
+}
+criacaoProdutos();
