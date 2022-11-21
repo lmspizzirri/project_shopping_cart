f0521767e0aca1ce17127c6ea2c7707ba122b5b1
lmspizzirri <lm.spizzirri@gmail.com>
2022-11-21 07:26:54 -0500

    Req 4

--- src/main.js
+++ src/main.js
@@ -4,6 +4,19 @@ import { createProductElement } from './helpers/shopFunctions';
 import './style.css';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+const loadMessage = document.createElement('p');
+const addLoad = () => {
+    const products = document.querySelector('.products');
+    loadMessage.className = 'loading';
+    loadMessage.innerText = 'carregando...'
+    products.appendChild(loadMessage);
+}
+addLoad();
+console.log(loadMessage);
+const removeLoad = (element) => {
+    const products = document.querySelector('.products');
+    products.removeChild(loadMessage);
+}
 const criacaoProdutos = async () => {
     //Recebendo as info dos computadores
   const info = await fetchProductsList('computador');
@@ -11,5 +24,8 @@ const criacaoProdutos = async () => {
   const products = document.querySelector('.products');
     //Adicionando dentro da section, os elementos com as info dos produtos
   info.forEach((element) => products.appendChild(createProductElement(element)));
+  removeLoad();
 }
 criacaoProdutos();
+window.onload = () => {
+}
