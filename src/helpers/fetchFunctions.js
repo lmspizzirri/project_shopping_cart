a651f05b11cf7ad1f0eec2a564e980c06105e143
lmspizzirri <lm.spizzirri@gmail.com>
2022-11-21 05:01:15 -0500

    Req 1 e 2

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -1,7 +1,12 @@
 export const fetchProduct = () => {
-  // seu código aqui
+
 };
 
-export const fetchProductsList = () => {
-  // seu código aqui
+export const fetchProductsList =  async(parameter) => {
+  if(!parameter){
+    throw new Error ('Termo de busca não informado')
+  }
+  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${parameter}`);
+  const data = await response.json();
+  return (data.results);
 };
