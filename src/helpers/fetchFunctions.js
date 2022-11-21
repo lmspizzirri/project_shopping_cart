d4e3e86557551986b9a6e3746d81ed3712757320
lmspizzirri <lm.spizzirri@gmail.com>
2022-11-21 07:55:04 -0500

    Req 7

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -1,10 +1,15 @@
-export const fetchProduct = () => {
-
+export const fetchProduct = async(id) => {
+  if(!id){
+    throw new Error ('ID não informado');
+  }
+  const response = await fetch(`https://api.mercadolibre.com/items/${id}`);
+  const data = await response.json();
+  return (data);
 };
 
 export const fetchProductsList =  async(parameter) => {
   if(!parameter){
-    throw new Error ('Termo de busca não informado')
+    throw new Error ('Termo de busca não informado');
   }
   const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${parameter}`);
   const data = await response.json();
